27944b9c848e02fcfcb0245276ccd362b83e5743
mkdeveloper <khubaib.mk@gmail.com>
2023-02-24 19:02:00 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
+  }
+  
 }
 
 module.exports = nextConfig
